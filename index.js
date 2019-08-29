1beec015320cbe5a62a05e0fe6677d1cbe3b55cd
Daniel Tonon <daniel.tonon.503@gmail.com>
2019-08-30 07:43:44 +1000

    Created the switch function

---
+++ index.js
@@ -0,0 +1,11 @@
+
+export default function switchFn(testCase = 'default', possibilities = {}){
+	if (!possibilities[testCase]) {
+		if (possibilities.default) {
+			return possibilities.default()
+		} else {
+			throw new Error(`"${testCase}" could not be found in the possibilities object (second parameter) and no default was defined`)
+		}
+	}
+	return possibilities[testCase]()
+}
