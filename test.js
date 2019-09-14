6c59ade6c0b3e7ed5f6d7c83e0d15f47a448dfee
Daniel Tonon <daniel.tonon@oakton.com.au>
2019-09-14 23:26:16 +1000

    Updated the tests js file to cover more possible scenarios and cover the new functionality
    I also improved the test output to be more informative

--- test.js
+++ test.js
@@ -1,29 +1,56 @@
 
 const Switch = require('./index')
 
-const test = testCase => Switch(testCase, {
-	one(){
-		return 'one'
-	},
-	'two two'(){
-		return 'two'
-	},
-	3(){
-		return 'three'
-	},
-	default(){
-		return 'default'
-	}
-})
+const example = 'example'
 
-const testStrings = ['one', 'two two', 3, 'does not exist']
+const possibilities = {
+	zero: 0,
+
+	nothing: null,
+
+	not_defined: undefined,
+
+	one: 'A',
+
+	[`two ${example} two`]: 'B',
+
+	'function call'(){
+		return 'with explicit return'
+	},
+
+	implicitFunction: ()=> 'implicit return',
+
+	default: 'default return value'
+}
+
+const expectation = [
+	0,
+	null,
+	'default return value',
+	'A',
+	'B',
+	'with explicit return',
+	'implicit return',
+	'default return value',
+	'default return value',
+]
+
+const test = testCase => Switch(testCase, possibilities)
+
+const testStrings = [...Object.keys(possibilities), 'does not exist']
 
 const tests = testStrings.map(string => test(string))
 
 const matchesExpectation = (test, expectation) => test.map((item, index) => item == expectation[index])
 
-const result = matchesExpectation(tests, ['one', 'two', 'three', 'default'])
+const result = matchesExpectation(tests, expectation)
 
-console.log(`Success? ${!result.includes(false)}`)
-console.log(`Result: [${result}]`)
-console.log(tests)
+console.log(`\nResult:`)
+
+console.log(testStrings.reduce((obj, item, i) => {
+	const resultView = !result[i] ? [result[i], expectation[i]] : result[i]
+	obj[item] = { [tests[i]]: resultView }
+	return obj
+}, {}))
+
+console.log(`\nSuccess?`, !result.includes(false))
