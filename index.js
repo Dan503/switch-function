166082b517def3a81b76e0d0dbd717ab5cf34a2d
Daniel Tonon <daniel.tonon@oakton.com.au>
2019-09-14 21:25:18 +1000

    Adding non-function syntax support
    It needed a bit of a refactor as well to not mistake falsy values as being undefined

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 
 module.exports = function switchFn(testCase = 'default', possibilities = {}){
-	if (!possibilities[testCase]) {
-		if (possibilities.default) {
-			return possibilities.default()
-		} else {
-			throw new Error(`"${testCase}" could not be found in the possibilities object (second parameter) and no default was defined`)
-		}
+
+	const testCaseFound = typeof possibilities[testCase] !== 'undefined'
+	const defaultIsSet = typeof possibilities.default !== 'undefined'
+
+	if (!testCaseFound && !defaultIsSet){
+		console.error('possibilities', possibilities)
+		throw Error(`Test case "${testCase}" could not be found in the possibilities object (second parameter) and no default was defined`)
 	}
-	return possibilities[testCase]()
+
+	const usingDefault = !testCaseFound && defaultIsSet
+
+	const testSubject = usingDefault ? 'default' : testCase
+
+	const isFn = typeof possibilities[testSubject] === 'function'
+
+	return isFn ? possibilities[testSubject]() : possibilities[testSubject]
 }
